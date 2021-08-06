1260da2e373a295be621c9bfa3dd7c951c7625df
bsullit <brian.sullivan.ic@gmail.com>
2021-08-06 19:55:01 +0000

    bash.js pair exersise completed to pwd functionality

---
+++ bash.js
@@ -0,0 +1,5 @@
+const pwdJs = module.require('./pwd.js');
+const fsJs = module.require('./ls.js');
+
+process.stdout.write('prompt >');
+
---
+++ pwd.js
@@ -0,0 +1,13 @@
+const pwdPrinter = process.stdin.on('data', (data) => {
+    let cmd = data.toString().trim();
+    
+   if (cmd === 'pwd'){
+       cmd = process.cwd();
+   }
+
+   
+    process.stdout.write('you typed: ' + cmd);
+    process.stdout.write('\n prompt');
+});
+
+module.export = pwdPrinter;
